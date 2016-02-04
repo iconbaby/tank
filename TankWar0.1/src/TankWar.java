228dfc3e8c3d89fb3f8098aabeb170975a1cbca4
iconbaby <644187060@qq.com>
2016-02-04 13:02:36 +0800

    0.6

--- TankWar0.1/src/TankWar.java
+++ TankWar0.1/src/TankWar.java
@@ -1,25 +1,32 @@
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 public class TankWar extends Frame{
-	int x = 50;
-	int y = 50;
+	
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 600;
+	
+	int x = 50, y = 50;
+	
 	Image offScreenImage = null;
+	
 	public void paint(Graphics g) {
 		Color c = g.getColor();
 		g.setColor(Color.RED);
 		g.fillOval(x,y,30,30);
 		g.setColor(c);
-		y += 5;
+		
 	}
 	public void update(Graphics g) {
 		if(offScreenImage == null) {
-			offScreenImage = this.createImage(800, 600);
+			offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
 		}
 		Graphics gOffScreen = offScreenImage.getGraphics();
 		Color c = gOffScreen.getColor();
 		gOffScreen.setColor(Color.GREEN);
-		gOffScreen.fillRect(0, 0, 800,600);
+		gOffScreen.fillRect(0, 0,GAME_WIDTH,GAME_HEIGHT);
 		gOffScreen.setColor(c);
 		paint(gOffScreen);
 		g.drawImage(offScreenImage,0,0,null);
@@ -32,7 +39,7 @@ public class TankWar extends Frame{
 	}
 	
 	public void LanuchFrame() {
-		this.setBounds(400,300,800,600);
+		this.setBounds(400,300,GAME_WIDTH,GAME_HEIGHT);
 		this.setResizable(false);
 		this.setTitle("TankWar");
 		this.setBackground(Color.GREEN);
@@ -43,6 +50,7 @@ public class TankWar extends Frame{
 				System.exit(0);
 			}
 		});
+		this.addKeyListener(new KeyMontor());
 		setVisible(true);
 		new Thread(new PaintThread()).start();
 		
@@ -53,13 +61,39 @@ public class TankWar extends Frame{
 			while (true) {
 				repaint();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(5);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 
 			}
 
+		}
+	}
+	private class KeyMontor extends KeyAdapter {
+
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_RIGHT:{  
+				x += 5;
+				break;
+			}
+			case KeyEvent.VK_LEFT:{
+				x -= 5;
+				break;
+			}
+			case KeyEvent.VK_UP: {
+				y -= 5;
+				break;
+			}
+			case KeyEvent.VK_DOWN: {
+				y += 5;
+				break;
+			}
+			
+			}
+				
 		}
 	}
 }
